78743e7d34af835f6c4023502fc492e01aaad361
Polixyner <117884364+Polixyner@users.noreply.github.com>
2023-12-03 23:40:42 +0700

    update  company

--- application/models/M_Supplier.php
+++ application/models/M_Supplier.php
@@ -16,9 +16,21 @@ class M_Supplier extends CI_Model {
 
     function getWhereIdSupplier($id){
         $query = $this->db->get_where('supplier', array('iduser' => $id));
-        return $query->row();
+        return $query->result();
     }
 
+    function getWhereIdCompany($id){
+        $query = $this->db->get_where('company', array('iduser' => $id));
+        return $query->result();
+    }
+
+    function insertCompany($data){
+        $this->db->insert('company',$data);
+    }
+
+    function updateCompany($id,$data){
+        $this->db->update('company', $data, array('iduser' => $id));
+    }
 }
 
 /* End of file M_Supplier.php */
