2a8712b2f2dd6af6358d0bc912c1e86dd720e3b9
egiofani <egiofani99@gmail.com>
2023-12-03 20:43:37 +0700

    fix navbar

--- application/views/componen/header.php
+++ application/views/componen/header.php
@@ -4,81 +4,92 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="An impressive and flawless site template that includes various UI elements and countless features, attractive ready-made blocks and rich pages, basically everything you need to create a unique and professional website.">
-  <meta name="keywords" content="bootstrap 5, business, corporate, creative, gulp, marketing, minimal, modern, multipurpose, one page, responsive, saas, sass, seo, startup, html5 template, site template">
+  <meta name="description"
+    content="An impressive and flawless site template that includes various UI elements and countless features, attractive ready-made blocks and rich pages, basically everything you need to create a unique and professional website.">
+  <meta name="keywords"
+    content="bootstrap 5, business, corporate, creative, gulp, marketing, minimal, modern, multipurpose, one page, responsive, saas, sass, seo, startup, html5 template, site template">
   <meta name="author" content="elemis">
   <title>Sandbox - Modern & Multipurpose Bootstrap 5 Template</title>
-  <link rel="shortcut icon" href="<?= base_url()?>./assets/front/img/favicon.png">
-  <link rel="stylesheet" href="<?= base_url()?>./assets/front/css/plugins.css">
-  <link rel="stylesheet" href="<?= base_url()?>./assets/front/css/style.css">
-  <link rel="stylesheet" href="<?= base_url()?>./assets/front/css/colors/yellow.css">
-  <link rel="preload" href="<?= base_url()?>./assets/front/css/fonts/thicccboi.css" as="style" onload="this.rel='stylesheet'">
+  <link rel="shortcut icon" href="<?= base_url() ?>./assets/front/img/favicon.png">
+  <link rel="stylesheet" href="<?= base_url() ?>./assets/front/css/plugins.css">
+  <link rel="stylesheet" href="<?= base_url() ?>./assets/front/css/style.css">
+  <link rel="stylesheet" href="<?= base_url() ?>./assets/front/css/colors/yellow.css">
+  <link rel="preload" href="<?= base_url() ?>./assets/front/css/fonts/thicccboi.css" as="style"
+    onload="this.rel='stylesheet'">
+
+    <style>
+      .language-select .dropdown-menu{
+        left: -50% !important;
+      }
+    </style>
 <body>
   <div class="content-wrapper">
     <header class="wrapper bg-soft-primary">
       <nav class="navbar navbar-expand-lg center-nav transparent navbar-light">
         <div class="container flex-lg-row flex-nowrap align-items-center">
           <div class="navbar-brand w-100">
-            <a href="<?= base_url()?>">
-              <img src="<?= base_url()?>./assets/front/img/logo-dark.png" srcset="<?= base_url()?>./assets/front/img/logo-dark@2x.png 2x" alt="" />
+            <a href="<?= base_url() ?>">
+              <img src="<?= base_url() ?>./assets/front/img/logo-dark.png"
+                srcset="<?= base_url() ?>./assets/front/img/logo-dark@2x.png 2x" alt="" />
             </a>
           </div>
           <div class="navbar-collapse offcanvas offcanvas-nav offcanvas-start">
             <div class="offcanvas-header d-lg-none">
               <h3 class="text-white fs-30 mb-0">Sandbox</h3>
-              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                aria-label="Close"></button>
             </div>
             <div class="offcanvas-body ms-lg-auto d-flex flex-column h-100">
               <ul class="navbar-nav">
                 <li class="nav-item ">
-                  <a class="nav-link dropdown-item" href="<?= base_url()?>/front/product" >Product</a>
+                  <a class="nav-link dropdown-item" href="<?= base_url() ?>/front/product">Product</a>
                 </li>
                 <li class="nav-item ">
-                  <a class="nav-link dropdown-item" href="<?= base_url()?>/front/supplier" >Supplier</a>
+                  <a class="nav-link dropdown-item" href="<?= base_url() ?>/front/supplier">Supplier</a>
                 </li>
                 <li class="nav-item dropdown dropdown-mega">
-                    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Category</a>
-                    <ul class="dropdown-menu mega-menu">
-                      <li class="mega-menu-content">
-                        <div class="row gx-0 gx-lg-3">
-                          <div class="col-lg-4">
-                            <h6 class="dropdown-header">Coconut</h6>
-                            <ul class="list-unstyled cc-2 pb-lg-1">
-                              <li><a class="dropdown-item" href="./docs/index.html">Bricket</a></li>
-                              <li><a class="dropdown-item" href="./docs/forms.html">Cocopeat</a></li>
-                              <li><a class="dropdown-item" href="./docs/faq.html">Coco-Fiber</a></li>
-                            </ul>
-                            <h6 class="dropdown-header mt-lg-6">Food and Agricultural</h6>
-                            <ul class="list-unstyled cc-2">
-                              <li><a class="dropdown-item" href="./docs/styleguide/colors.html">Apple</a></li>
-                              <li><a class="dropdown-item" href="./docs/styleguide/fonts.html">Cinamon</a></li>
-                              <li><a class="dropdown-item" href="./docs/styleguide/icons-svg.html">Penapple</a></li>
-                              <li><a class="dropdown-item" href="./docs/styleguide/colors.html">Apple</a></li>
-                              <li><a class="dropdown-item" href="./docs/styleguide/fonts.html">Cinamon</a></li>
-                              <li><a class="dropdown-item" href="./docs/styleguide/icons-svg.html">Penapple</a></li>
-                            </ul>
-                          </div>
-                          <!--/column -->
-                          <div class="col-lg-8">
-                            <h6 class="dropdown-header">Sea & Water</h6>
-                            <ul class="list-unstyled cc-3">
-                              <li><a class="dropdown-item" href="./docs/elements/accordion.html">Sarder</a></li>
-                              <li><a class="dropdown-item" href="./docs/elements/alerts.html">Ikan Kering</a></li>
-                              <li><a class="dropdown-item" href="./docs/elements/animations.html">Fish Oil</a></li>
-                              <li><a class="dropdown-item" href="./docs/elements/accordion.html">Sarder</a></li>
-                              <li><a class="dropdown-item" href="./docs/elements/alerts.html">Ikan Kering</a></li>
-                              <li><a class="dropdown-item" href="./docs/elements/animations.html">Fish Oil</a></li>
-                              <li><a class="dropdown-item" href="./docs/elements/avatars.html"></a></li>
-                            </ul>
-                          </div>
-                          <!--/column -->
+                  <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Category</a>
+                  <ul class="dropdown-menu mega-menu">
+                    <li class="mega-menu-content">
+                      <div class="row gx-0 gx-lg-3">
+                        <div class="col-lg-4">
+                          <h6 class="dropdown-header">Coconut</h6>
+                          <ul class="list-unstyled cc-2 pb-lg-1">
+                            <li><a class="dropdown-item" href="./docs/index.html">Bricket</a></li>
+                            <li><a class="dropdown-item" href="./docs/forms.html">Cocopeat</a></li>
+                            <li><a class="dropdown-item" href="./docs/faq.html">Coco-Fiber</a></li>
+                          </ul>
+                          <h6 class="dropdown-header mt-lg-6">Food and Agricultural</h6>
+                          <ul class="list-unstyled cc-2">
+                            <li><a class="dropdown-item" href="./docs/styleguide/colors.html">Apple</a></li>
+                            <li><a class="dropdown-item" href="./docs/styleguide/fonts.html">Cinamon</a></li>
+                            <li><a class="dropdown-item" href="./docs/styleguide/icons-svg.html">Penapple</a></li>
+                            <li><a class="dropdown-item" href="./docs/styleguide/colors.html">Apple</a></li>
+                            <li><a class="dropdown-item" href="./docs/styleguide/fonts.html">Cinamon</a></li>
+                            <li><a class="dropdown-item" href="./docs/styleguide/icons-svg.html">Penapple</a></li>
+                          </ul>
                         </div>
-                        <!--/.row -->
-                      </li>
-                      <!--/.mega-menu-content-->
-                    </ul>
-                    <!--/.dropdown-menu -->
-                  </li>
+                        <!--/column -->
+                        <div class="col-lg-8">
+                          <h6 class="dropdown-header">Sea & Water</h6>
+                          <ul class="list-unstyled cc-3">
+                            <li><a class="dropdown-item" href="./docs/elements/accordion.html">Sarder</a></li>
+                            <li><a class="dropdown-item" href="./docs/elements/alerts.html">Ikan Kering</a></li>
+                            <li><a class="dropdown-item" href="./docs/elements/animations.html">Fish Oil</a></li>
+                            <li><a class="dropdown-item" href="./docs/elements/accordion.html">Sarder</a></li>
+                            <li><a class="dropdown-item" href="./docs/elements/alerts.html">Ikan Kering</a></li>
+                            <li><a class="dropdown-item" href="./docs/elements/animations.html">Fish Oil</a></li>
+                            <li><a class="dropdown-item" href="./docs/elements/avatars.html"></a></li>
+                          </ul>
+                        </div>
+                        <!--/column -->
+                      </div>
+                      <!--/.row -->
+                    </li>
+                    <!--/.mega-menu-content-->
+                  </ul>
+                  <!--/.dropdown-menu -->
+                </li>
               </ul>
               <!-- /.navbar-nav -->
               <div class="offcanvas-footer d-lg-none">
@@ -102,27 +113,33 @@
           <!-- /.navbar-collapse -->
           <div class="navbar-other w-100 d-flex ms-auto">
             <ul class="navbar-nav flex-row align-items-center ms-auto">
+            <li class="nav-item d-lg-none">
+                <button class="hamburger offcanvas-nav-btn"><span></span></button>
+              </li>
               <?php $username = $this->session->userdata('username');
-                if ($username) : ?>
-                  <li class="nav-item dropdown text-uppercase">
-                    <a class="dropdown-item dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true"><?= $this->session->userdata('username')?></a>
-                    <ul class="dropdown-menu show" data-bs-popper="static">
-                      <li class="nav-item"><a class="dropdown-item" href="<?=base_url()?>profil">Profil</a></li>
-                      <li class="nav-item"><a class="dropdown-item" href="<?=base_url()?>supplier">Dashbord</a></li>
-                      <li class="nav-item"><a class="dropdown-item" href="<?=base_url()?>logout">Logout</a></li>
-                    </ul>
-                  </li>
-                <?php else : ?>
-                  <li class="nav-item">
-                    <a href="<?=base_url()?>login" class="nav-link" >Sign In</a>
-                  </li>
-                  <li class="nav-item d-none d-md-block">
-                    <a href="<?=base_url()?>register" class="btn btn-sm btn-primary rounded" >Sign Up</a>
-                  </li>
-                <?php endif; ?>
-                  <li class="nav-item d-lg-none">
-                    <button class="hamburger offcanvas-nav-btn"><span></span></button>
-                  </li>
+              if ($username): ?>
+                <li class="nav-item dropdown language-select">
+                  <a class="nav-link text-dark dropdown-item dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    <img class="avatar w-8" src="<?= base_url('') ?>assets/admin/media/avatars/avatar10.jpg" alt="" />
+                    <!-- <i class="uil uil-user"></i> -->
+                  </a>
+                  <ul class="dropdown-menu ">
+                    <!-- IF already creator -->
+                    <li class="nav-item"><a class="dropdown-item" href="<?= base_url() ?>supplier">Dashboard</a></li>
+                    <!-- Else -->
+                    <li class="nav-item"><a class="dropdown-item" href="<?= base_url() ?>profil">Profile</a></li>
+                    <li class="nav-item"><a class="dropdown-item" href="<?= base_url() ?>logout">Logout</a></li>
+                  </ul>
+                </li>
+              <?php else: ?>
+                <li class="nav-item">
+                  <a href="<?= base_url() ?>login" class="nav-link">Sign In</a>
+                </li>
+                <li class="nav-item d-none d-md-block">
+                  <a href="<?= base_url() ?>register" class="btn btn-sm btn-primary rounded">Sign Up</a>
+                </li>
+              <?php endif; ?>
             </ul>
             <!-- /.navbar-nav -->
           </div>
@@ -134,11 +151,13 @@
       <div class="offcanvas offcanvas-end text-inverse" id="offcanvas-info" data-bs-scroll="true">
         <div class="offcanvas-header">
           <h3 class="text-white fs-30 mb-0">Sandbox</h3>
-          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+            aria-label="Close"></button>
         </div>
         <div class="offcanvas-body pb-6">
           <div class="widget mb-8">
-            <p>Sandbox is a multipurpose HTML5 template with various layouts which will be a great solution for your business.</p>
+            <p>Sandbox is a multipurpose HTML5 template with various layouts which will be a great solution for your
+              business.</p>
           </div>
           <!-- /.widget -->
           <div class="widget mb-8">
