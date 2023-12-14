5b2804b44e5c8fbc8b746cca5f831bbcd1da7b71
egiofani <egiofani99@gmail.com>
2023-12-14 23:23:22 +0700

    tambah view  Forum

--- application/views/detailPost.php
+++ application/views/detailPost.php
@@ -1,131 +1,193 @@
 <?php $this->load->view($header); ?>
-
-<section class="wrapper bg-gradient-primary">
-      <div class="container pt-10 pb-10 pt-md-5 pb-md-15 text-center">
-      <div id="comments">
-                    <h3 class="mb-6">5 Comments</h3>
-                    <ol id="singlecomments" class="commentlist">
-                      <li class="comment">
-                        <div class="comment-header d-md-flex align-items-center">
-                          <div class="d-flex align-items-center">
-                            <figure class="user-avatar"><img class="rounded-circle" alt="" src="./assets/img/avatars/u1.jpg" /></figure>
-                            <div>
-                              <h6 class="comment-author"><a href="#" class="link-dark">Connor Gibson</a></h6>
-                              <ul class="post-meta">
-                                <li><i class="uil uil-calendar-alt"></i>14 Jan 2022</li>
-                              </ul>
-                              <!-- /.post-meta -->
-                            </div>
-                            <!-- /div -->
-                          </div>
-                          <!-- /div -->
-                          <div class="mt-3 mt-md-0 ms-auto">
-                            <a href="#" class="btn btn-soft-ash btn-sm rounded-pill btn-icon btn-icon-start mb-0"><i class="uil uil-comments"></i> Reply</a>
-                          </div>
-                          <!-- /div -->
-                        </div>
-                        <!-- /.comment-header -->
-                        <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Sed posuere consectetur est at lobortis integer posuere erat ante.</p>
-                      </li>
-                      <li class="comment">
-                        <div class="comment-header d-md-flex align-items-center">
-                          <div class="d-flex align-items-center">
-                            <figure class="user-avatar"><img class="rounded-circle" alt="" src="./assets/img/avatars/u2.jpg" /></figure>
-                            <div>
-                              <h6 class="comment-author"><a href="#" class="link-dark">Nikolas Brooten</a></h6>
-                              <ul class="post-meta">
-                                <li><i class="uil uil-calendar-alt"></i>21 Feb 2022</li>
-                              </ul>
-                              <!-- /.post-meta -->
-                            </div>
-                            <!-- /div -->
-                          </div>
-                          <!-- /div -->
-                          <div class="mt-3 mt-md-0 ms-auto">
-                            <a href="#" class="btn btn-soft-ash btn-sm rounded-pill btn-icon btn-icon-start mb-0"><i class="uil uil-comments"></i> Reply</a>
-                          </div>
-                          <!-- /div -->
-                        </div>
-                        <!-- /.comment-header -->
-                        <p>Quisque tristique tincidunt metus non aliquam. Quisque ac risus sit amet quam sollicitudin vestibulum vitae malesuada libero. Mauris magna elit, suscipit non ornare et, blandit a tellus. Pellentesque dignissim ornare faucibus mollis.</p>
-                        <ul class="children">
-                          <li class="comment">
-                            <div class="comment-header d-md-flex align-items-center">
-                              <div class="d-flex align-items-center">
-                                <figure class="user-avatar"><img class="rounded-circle" alt="" src="./assets/img/avatars/u3.jpg" /></figure>
-                                <div>
-                                  <h6 class="comment-author"><a href="#" class="link-dark">Pearce Frye</a></h6>
-                                  <ul class="post-meta">
-                                    <li><i class="uil uil-calendar-alt"></i>22 Feb 2022</li>
-                                  </ul>
-                                  <!-- /.post-meta -->
-                                </div>
-                                <!-- /div -->
-                              </div>
-                              <!-- /div -->
-                              <div class="mt-3 mt-md-0 ms-auto">
-                                <a href="#" class="btn btn-soft-ash btn-sm rounded-pill btn-icon btn-icon-start mb-0"><i class="uil uil-comments"></i> Reply</a>
-                              </div>
-                              <!-- /div -->
-                            </div>
-                            <!-- /.comment-header -->
-                            <p>Cras mattis consectetur purus sit amet fermentum. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Etiam porta sem malesuada magna mollis.</p>
-                            <ul class="children">
-                              <li class="comment">
-                                <div class="comment-header d-md-flex align-items-center">
-                                  <div class="d-flex align-items-center">
-                                    <figure class="user-avatar"><img class="rounded-circle" alt="" src="./assets/img/avatars/u2.jpg" /></figure>
-                                    <div>
-                                      <h6 class="comment-author"><a href="#" class="link-dark">Nikolas Brooten</a></h6>
-                                      <ul class="post-meta">
-                                        <li><i class="uil uil-calendar-alt"></i>4 Apr 2022</li>
-                                      </ul>
-                                      <!-- /.post-meta -->
-                                    </div>
-                                    <!-- /div -->
-                                  </div>
-                                  <!-- /div -->
-                                  <div class="mt-3 mt-md-0 ms-auto">
-                                    <a href="#" class="btn btn-soft-ash btn-sm rounded-pill btn-icon btn-icon-start mb-0"><i class="uil uil-comments"></i> Reply</a>
-                                  </div>
-                                  <!-- /div -->
-                                </div>
-                                <!-- /.comment-header -->
-                                <p>Nullam id dolor id nibh ultricies vehicula ut id. Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia aenean bibendum nulla consectetur.</p>
-                              </li>
-                            </ul>
-                          </li>
-                        </ul>
-                      </li>
-                      <li class="comment">
-                        <div class="comment-header d-md-flex align-items-center">
-                          <div class="d-flex align-items-center">
-                            <figure class="user-avatar"><img class="rounded-circle" alt="" src="./assets/img/avatars/u4.jpg" /></figure>
-                            <div>
-                              <h6 class="comment-author"><a href="#" class="link-dark">Lou Bloxham</a></h6>
-                              <ul class="post-meta">
-                                <li><i class="uil uil-calendar-alt"></i>3 May 2022</li>
-                              </ul>
-                              <!-- /.post-meta -->
-                            </div>
-                            <!-- /div -->
-                          </div>
-                          <!-- /div -->
-                          <div class="mt-3 mt-md-0 ms-auto">
-                            <a href="#" class="btn btn-soft-ash btn-sm rounded-pill btn-icon btn-icon-start mb-0"><i class="uil uil-comments"></i> Reply</a>
-                          </div>
-                          <!-- /div -->
-                        </div>
-                        <!-- /.comment-header -->
-                        <p>Sed posuere consectetur est at lobortis. Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                      </li>
-                    </ol>
-                  </div>
-                  <!-- /#comments -->
+<section class="wrapper bg-gradient-primary pt-10">
+  <div class="container rounded bg-white pt-10 px-md-10 pb-10 pt-md-5 pb-md-15 ">
+    <div class="author-info d-md-flex align-items-center mb-3">
+      <div class="d-flex align-items-center">
+        <figure class="user-avatar"><img class="rounded-circle" alt="" src="<?= base_url()?>./assets/front/img/avatars/te1.jpg" /></figure>
+        <div>
+          <h6><a href="#" class="link-dark">Nikolas Brooten</a></h6>
+          <span class="post-meta fs-15">PT Jumali Ahmad</span>
+        </div>
       </div>
-    <!-- /.container -->
+      <div class="mt-3 mt-md-0 ms-auto">
+              <a href="#" class="btn btn-soft-ash  btn-sm rounded-pill btn-icon btn-icon-start mb-0" data-bs-toggle="modal" data-bs-target="#modal-signin"><i
+                  class="uil uil-comments"></i> Reply</a>
+            </div>
+    </div>
+    <!-- /.author-info -->
+    <p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+      Maecenas faucibus mollis interdum. Fusce dapibus, tellus ac. Maecenas faucibus mollis interdum.</p>
+    <nav class="nav social mb-n12">
+      <p>14 Jan 2022</p>
+    </nav>
+    <!-- /.social -->
+    <hr />
+    <div class="" id="comments">
+      <h3 class="mb-6">5 Comments</h3>
+      <ol id="singlecomments" class="commentlist">
+        <li class="comment">
+          <div class="comment-header d-md-flex align-items-center">
+            <div class="d-flex align-items-center">
+              <figure class="user-avatar"><img class="rounded-circle" alt="" src="<?= base_url()?>./assets/front/img/avatars/te1.jpg" />
+              </figure>
+              <div>
+                <h6 class="comment-author"><a href="#" class="link-dark">Connor Gibson</a></h6>
+                <ul class="post-meta">
+                  <li><i class="uil uil-calendar-alt"></i>14 Jan 2022</li>
+                </ul>
+                <!-- /.post-meta -->
+              </div>
+              <!-- /div -->
+            </div>
+            <!-- /div -->
+            <div class="mt-3 mt-md-0 ms-auto">
+              <a href="#" class="btn btn-soft-ash  btn-sm rounded-pill btn-icon btn-icon-start mb-0" data-bs-toggle="modal" data-bs-target="#modal-signin"><i
+                  class="uil uil-comments"></i> Reply</a>
+            </div>
+            <!-- /div -->
+          </div>
+          <!-- /.comment-header -->
+          <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo
+            luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Sed posuere consectetur est at lobortis
+            integer posuere erat ante.</p>
+        </li>
+        <li class="comment">
+          <div class="comment-header d-md-flex align-items-center">
+            <div class="d-flex align-items-center">
+              <figure class="user-avatar"><img class="rounded-circle" alt="" src="<?= base_url()?>./assets/front/img/avatars/te1.jpg" />
+              </figure>
+              <div>
+                <h6 class="comment-author"><a href="#" class="link-dark">Nikolas Brooten</a></h6>
+                <ul class="post-meta">
+                  <li><i class="uil uil-calendar-alt"></i>21 Feb 2022</li>
+                </ul>
+                <!-- /.post-meta -->
+              </div>
+              <!-- /div -->
+            </div>
+            <!-- /div -->
+            <div class="mt-3 mt-md-0 ms-auto">
+              <a href="#" class="btn btn-soft-ash  btn-sm rounded-pill btn-icon btn-icon-start mb-0" data-bs-toggle="modal" data-bs-target="#modal-signin"><i
+                  class="uil uil-comments"></i> Reply</a>
+            </div>
+            <!-- /div -->
+          </div>
+          <!-- /.comment-header -->
+          <p>Quisque tristique tincidunt metus non aliquam. Quisque ac risus sit amet quam sollicitudin vestibulum vitae
+            malesuada libero. Mauris magna elit, suscipit non ornare et, blandit a tellus. Pellentesque dignissim ornare
+            faucibus mollis.</p>
+          <ul class="children">
+            <li class="comment">
+              <div class="comment-header d-md-flex align-items-center">
+                <div class="d-flex align-items-center">
+                  <figure class="user-avatar"><img class="rounded-circle" alt="" src="<?= base_url()?>./assets/front/img/avatars/te1.jpg" />
+                  </figure>
+                  <div>
+                    <h6 class="comment-author"><a href="#" class="link-dark">Pearce Frye</a></h6>
+                    <ul class="post-meta">
+                      <li><i class="uil uil-calendar-alt"></i>22 Feb 2022</li>
+                    </ul>
+                    <!-- /.post-meta -->
+                  </div>
+                  <!-- /div -->
+                </div>
+                <!-- /div -->
+                <div class="mt-3 mt-md-0 ms-auto">
+                  <a href="#" class="btn btn-soft-ash  btn-sm rounded-pill btn-icon btn-icon-start mb-0" data-bs-toggle="modal" data-bs-target="#modal-signin"><i
+                      class="uil uil-comments"></i> Reply</a>
+                </div>
+                <!-- /div -->
+              </div>
+              <!-- /.comment-header -->
+              <p>Cras mattis consectetur purus sit amet fermentum. Integer posuere erat a ante venenatis dapibus posuere
+                velit aliquet. Etiam porta sem malesuada magna mollis.</p>
+              <ul class="children">
+                <li class="comment">
+                  <div class="comment-header d-md-flex align-items-center">
+                    <div class="d-flex align-items-center">
+                      <figure class="user-avatar"><img class="rounded-circle" alt=""
+                          src="<?= base_url()?>./assets/front/img/avatars/te1.jpg" /></figure>
+                      <div>
+                        <h6 class="comment-author"><a href="#" class="link-dark">Nikolas Brooten</a></h6>
+                        <ul class="post-meta">
+                          <li><i class="uil uil-calendar-alt"></i>4 Apr 2022</li>
+                        </ul>
+                        <!-- /.post-meta -->
+                      </div>
+                      <!-- /div -->
+                    </div>
+                    <!-- /div -->
+                    <div class="mt-3 mt-md-0 ms-auto">
+                      <a href="#" class="btn btn-soft-ash  btn-sm rounded-pill btn-icon btn-icon-start mb-0" data-bs-toggle="modal" data-bs-target="#modal-signin"><i
+                          class="uil uil-comments"></i> Reply</a>
+                    </div>
+                    <!-- /div -->
+                  </div>
+                  <!-- /.comment-header -->
+                  <p>Nullam id dolor id nibh ultricies vehicula ut id. Cras mattis consectetur purus sit amet fermentum.
+                    Aenean eu leo quam. Pellentesque ornare sem lacinia aenean bibendum nulla consectetur.</p>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li class="comment">
+          <div class="comment-header d-md-flex align-items-center">
+            <div class="d-flex align-items-center">
+              <figure class="user-avatar"><img class="rounded-circle" alt="" src="<?= base_url()?>./assets/front/img/avatars/te1.jpg" />
+              </figure>
+              <div>
+                <h6 class="comment-author"><a href="#" class="link-dark">Lou Bloxham</a></h6>
+                <ul class="post-meta">
+                  <li><i class="uil uil-calendar-alt"></i>3 May 2022</li>
+                </ul>
+                <!-- /.post-meta -->
+              </div>
+              <!-- /div -->
+            </div>
+            <!-- /div -->
+            <div class="mt-3 mt-md-0 ms-auto">
+              <a href="#" class="btn btn-soft-ash  btn-sm rounded-pill btn-icon btn-icon-start mb-0" data-bs-toggle="modal" data-bs-target="#modal-signin"><i
+                  class="uil uil-comments"></i> Reply</a>
+            </div>
+            <!-- /div -->
+          </div>
+          <!-- /.comment-header -->
+          <p>Sed posuere consectetur est at lobortis. Vestibulum id ligula porta felis euismod semper. Cum sociis
+            natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+        </li>
+      </ol>
+    </div>
+    <!-- /#comments -->
+  </div>
+  <!-- /.container -->
 </section>
-    <!-- /section -->
+<!-- /section -->
+
+<div class="modal fade" id="modal-signin" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+        <div class="modal-content text-center">
+            <div class="modal-body">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h2 class="mb-3 text-start">Reply</h2>
+                <p class="lead mb-6 text-start">Your message will be sent to the comment section</p>
+                <form class="text-start mb-3">
+                <div class="form-floating mb-4">
+                    <input type="text" class="form-control" placeholder="Email" value="" >
+                    <label for="loginEmail">Your Name</label>
+                </div>
+                <div class="form-floating mb-4">
+                    <textarea id="textareaExample" class="form-control" placeholder="Textarea" style="height: 150px" required></textarea>
+                    <label for="textareaExample">Message</label>
+                </div>
+                <a href="" class="btn btn-primary rounded-pill btn-login w-100 mb-2" >Send Message</a>
+                </form>
+                <!-- /form -->
+                <!--/.social -->
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php $this->load->view($footer); ?>
