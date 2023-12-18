3a0e6bf9e89e4261940660d7f67f6bcd5de0204f
egiofani <egiofani99@gmail.com>
2023-12-19 02:19:42 +0700

    fix home

--- application/views/home.php
+++ application/views/home.php
@@ -29,7 +29,7 @@
                   </div>
               </div>
               <div class="col-12 col-md-4 mt-3 mt-md-0">
-                  <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                  <div class="d-grid gap-1 d-md-flex justify-content-md-end">
                     <button class="btn btn-md btn-primary rounded me-md-2 mb-2 mb-md-0"  type="submit" name="search_type" value="supplier">Supplier</button>
                     <button class="btn btn-md btn-primary rounded" type="submit" name="search_type" value="product">Product</button>
                   </div>
@@ -193,7 +193,7 @@
     </section>
     <!-- /section -->
     <section class="wrapper bg-light angled upper-start lower-start">
-      <div class="container py-14 pt-md-17 pb-md-15">
+      <div class="container   pb-md-15">
         
       </div>
       <!-- /.container -->
@@ -332,78 +332,6 @@
     <!-- /section -->
     <section class="wrapper bg-light angled upper-end lower-end">
       <div class="container py-14 pt-md-14 pb-md-18">
-        <div class="row gy-6 mb-14 mb-md-18">
-          <div class="col-lg-4">
-            <h2 class="fs-16 text-uppercase text-muted mt-lg-18 mb-3">Our Pricing</h2>
-            <h3 class="display-4 mb-3">We offer great and premium prices.</h3>
-            <p>Enjoy a <a href="#" class="hover">free 30-day trial</a> and experience the full service. No credit card required!</p>
-            <a href="#" class="btn btn-primary rounded-pill mt-2">See All Prices</a>
-          </div>
-          <!--/column -->
-          <div class="col-lg-7 offset-lg-1 pricing-wrapper">
-            <div class="pricing-switcher-wrapper switcher justify-content-start justify-content-lg-end">
-              <p class="mb-0 pe-3">Monthly</p>
-              <div class="pricing-switchers">
-                <div class="pricing-switcher pricing-switcher-active"></div>
-                <div class="pricing-switcher"></div>
-                <div class="switcher-button bg-primary"></div>
-              </div>
-              <p class="mb-0 ps-3">Yearly <span class="text-red">(Save 30%)</span></p>
-            </div>
-            <div class="row gy-6 position-relative mt-5">
-              <div class="shape bg-dot red rellax w-16 h-18" data-rellax-speed="1" style="bottom: -0.5rem; right: -1.6rem;"></div>
-              <div class="col-md-6">
-                <div class="pricing card shadow-lg">
-                  <div class="card-body pb-12">
-                    <div class="prices text-dark">
-                      <div class="price price-show justify-content-start"><span class="price-currency">$</span><span class="price-value">19</span> <span class="price-duration">mo</span></div>
-                      <div class="price price-hide price-hidden justify-content-start"><span class="price-currency">$</span><span class="price-value">199</span> <span class="price-duration">yr</span></div>
-                    </div>
-                    <!--/.prices -->
-                    <h4 class="card-title mt-2">Premium Plan</h4>
-                    <ul class="icon-list bullet-primary mt-7 mb-8">
-                      <li><i class="uil uil-check fs-21"></i><span><strong>5</strong> Projects </span></li>
-                      <li><i class="uil uil-check fs-21"></i><span><strong>100K</strong> API Access </span></li>
-                      <li><i class="uil uil-check fs-21"></i><span><strong>200MB</strong> Storage </span></li>
-                      <li><i class="uil uil-check fs-21"></i><span> Weekly <strong>Reports</strong></span></li>
-                      <li><i class="uil uil-check fs-21"></i><span> 7/24 <strong>Support</strong></span></li>
-                    </ul>
-                    <a href="#" class="btn btn-primary rounded-pill">Choose Plan</a>
-                  </div>
-                  <!--/.card-body -->
-                </div>
-                <!--/.pricing -->
-              </div>
-              <!--/column -->
-              <div class="col-md-6 popular">
-                <div class="pricing card shadow-lg">
-                  <div class="card-body pb-12">
-                    <div class="prices text-dark">
-                      <div class="price price-show justify-content-start"><span class="price-currency">$</span><span class="price-value">49</span> <span class="price-duration">mo</span></div>
-                      <div class="price price-hide price-hidden justify-content-start"><span class="price-currency">$</span><span class="price-value">499</span> <span class="price-duration">yr</span></div>
-                    </div>
-                    <!--/.prices -->
-                    <h4 class="card-title mt-2">Corporate Plan</h4>
-                    <ul class="icon-list bullet-primary mt-7 mb-8">
-                      <li><i class="uil uil-check fs-21"></i><span><strong>20</strong> Projects </span></li>
-                      <li><i class="uil uil-check fs-21"></i><span><strong>300K</strong> API Access </span></li>
-                      <li><i class="uil uil-check fs-21"></i><span><strong>500MB</strong> Storage </span></li>
-                      <li><i class="uil uil-check fs-21"></i><span> Weekly <strong>Reports</strong></span></li>
-                      <li><i class="uil uil-check fs-21"></i><span> 7/24 <strong>Support</strong></span></li>
-                    </ul>
-                    <a href="#" class="btn btn-primary rounded-pill">Choose Plan</a>
-                  </div>
-                  <!--/.card-body -->
-                </div>
-                <!--/.pricing -->
-              </div>
-              <!--/column -->
-            </div>
-            <!--/.row -->
-          </div>
-          <!--/column -->
-        </div>
-        <!--/.row -->
         <div class="row gx-lg-8 gx-xl-12 gy-10 mb-10 mb-md-14 align-items-center">
           <div class="col-lg-7">
             <figure><img class="w-auto" src="<?= base_url()?>./assets/front/img/illustrations/i5.png" srcset="<?= base_url()?>./assets/front/img/illustrations/i5@2x.png 2x" alt="" /></figure>
